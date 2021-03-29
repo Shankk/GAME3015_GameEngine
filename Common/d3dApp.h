884bccd7e3fe000531a59bb28522a26189e02f1b
Brian <46753834+Shankk@users.noreply.github.com>
2021-03-29 18:33:16 -0400

    Quick Fix

--- Common/d3dApp.h
+++ Common/d3dApp.h
@@ -117,7 +117,7 @@ protected:
 	UINT mCbvSrvUavDescriptorSize = 0;
 
 	// Derived class should set these in derived constructor to customize starting values.
-	std::wstring mMainWndCaption = L"Michael Denkovski 101222288 Kevin Chua 101222467";
+	std::wstring mMainWndCaption = L"Brian Viveiros Emmanuel Nofuente-Loblack";
 	D3D_DRIVER_TYPE md3dDriverType = D3D_DRIVER_TYPE_HARDWARE;
     DXGI_FORMAT mBackBufferFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
     DXGI_FORMAT mDepthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
